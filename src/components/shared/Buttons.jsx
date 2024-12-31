a7a0992ca076334a70c2df6a653deccf790d3abc
prathamtshah <prathamtshah@thegatewaycorp.co.in>
2024-12-31 23:08:03 +0530

    fix:remove extra padding in custom button

--- src/components/shared/Buttons.jsx
+++ src/components/shared/Buttons.jsx
@@ -4,7 +4,7 @@ export const DonateButton = () => {
   return (
     <OutLink
       href="https://rzp.io/l/KzOI5A3"
-      className="inline-block py-2 font-bold text-white border-white rounded-md hover:border-primary hover:text-primary md:border-2 md:px-9">
+      className="inline-block rounded-md border-white py-2 font-bold text-white hover:border-primary hover:text-primary md:border-2 md:px-9">
       Donate
     </OutLink>
   );
@@ -13,7 +13,7 @@ export const DonateButton = () => {
 const Button = ({ children, outline, className, href }) => {
   return (
     <InLink
-      className={`inline-block rounded-md py-2  px-9 text-center font-semibold capitalize ring-2 ring-primary ${
+      className={`inline-block rounded-md py-2  text-center font-semibold capitalize ring-2 ring-primary ${
         outline ? ' text-primary hover:bg-[#EEF5FE]' : 'bg-primary text-white hover:bg-[#266ED2]'
       } ${className}`}
       href={href ? href : '#'}>
@@ -27,20 +27,20 @@ export default Button;
 export const AuthBtnGroup = ({ className }) => {
   return (
     <div className={`flex flex-col items-start lg:flex-row ${className}`}>
-      <button className="loginButton signup inline-block rounded-md py-2 px-8 text-center font-semibold capitalize text-primary ring-2 ring-primary hover:bg-[#EEF5FE]">
+      <button className="loginButton signup inline-block rounded-md px-8 py-2 text-center font-semibold capitalize text-primary ring-2 ring-primary hover:bg-[#EEF5FE]">
         sign up
       </button>
-      <button className="loginButton login mt-7 inline-block rounded-md bg-primary py-2 px-8 text-center font-semibold capitalize text-white ring-2 ring-primary hover:bg-[#266ED2] hover:ring-[#266ED2] lg:mt-0 lg:ml-10">
+      <button className="loginButton login mt-7 inline-block rounded-md bg-primary px-8 py-2 text-center font-semibold capitalize text-white ring-2 ring-primary hover:bg-[#266ED2] hover:ring-[#266ED2] lg:ml-10 lg:mt-0">
         log in
       </button>
 
       <button
-        className="inline-block px-8 py-2 font-semibold text-center text-white capitalize rounded-md postLogin goToAccountButton bg-primary ring-2 ring-primary"
+        className="postLogin goToAccountButton inline-block rounded-md bg-primary px-8 py-2 text-center font-semibold capitalize text-white ring-2 ring-primary"
         style={{ display: 'none' }}>
         Account
       </button>
       <button
-        className="inline-block px-8 py-2 font-semibold text-center text-red-900 capitalize rounded-md postLogin logout mt-7 ring-2 ring-red-200 lg:mt-0 lg:ml-10"
+        className="postLogin logout mt-7 inline-block rounded-md px-8 py-2 text-center font-semibold capitalize text-red-900 ring-2 ring-red-200 lg:ml-10 lg:mt-0"
         style={{ display: 'none' }}>
         Logout
       </button>
